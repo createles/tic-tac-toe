481370c12c22105d1e296eab20c9ab2d66891e5f
createlsworklappy <justinecastillo7@gmail.com>
2025-02-27 15:55:00 +0900

    Func: Start determining game logic on the console

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// Initializes a 2D gameboard, 
+// where each spot on the board is it's own array []
+// function Gameboard() {
+//     const columns = 3;
+//     const rows = 3;
+//     const board = [];
+
+//     for (let i = 0; i < rows; i++) {
+//         board[i] = [];
+//         for (let j = 0; j < columns; j++) {
+//             );
+//         }
+//     }
+// }
+
